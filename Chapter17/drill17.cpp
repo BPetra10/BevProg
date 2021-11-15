4b3d8b5d6373e9de931d305147529feb2dc9287b
Bodnár Petra <70892820+BPetra10@users.noreply.github.com>
2021-11-15 17:25:21 +0100

    Update drill17.cpp

--- Chapter17/drill17.cpp
+++ Chapter17/drill17.cpp
@@ -16,26 +16,26 @@ void print_array(ostream& os, int* a, int n)
 
 int main()
 {
-		//1-3.
-		int* array = new int[10];
-		print_array10(cout,array);
-		cout<<"\n";
-		delete[] array;
-		//5.
-			int* array2 = new int[10]{100,101,102,103,104,105,106,107,108,109};
-			print_array10(cout,array2);
-			cout<<"\n";
-		//6.
-			int* array3 = new int[11]{100,101,102,103,104,105,106,107,108,109,110};
-			print_array(cout,array3,11);
-			cout<<"\n";
-		//7. 
-		int* array4 = new int[20];
-		for (int i = 0; i < 20; i++)
-		{
-			array4[i]=100+i;
-		}
-		print_array(cout,array4,20);
+	//1-3.
+	int* array = new int[10];
+	print_array10(cout,array);
+	cout<<"\n";
+	delete[] array;
+	//5.
+	int* array2 = new int[10]{100,101,102,103,104,105,106,107,108,109};
+	print_array10(cout,array2);
+	cout<<"\n";
+	//6.
+	int* array3 = new int[11]{100,101,102,103,104,105,106,107,108,109,110};
+	print_array(cout,array3,11);
+	cout<<"\n";
+	//7. 
+	int* array4 = new int[20];
+	for (int i = 0; i < 20; i++)
+	{
+		array4[i]=100+i;
+	}
+	print_array(cout,array4,20);
 	
-	    return 0;
+	return 0;
 }
